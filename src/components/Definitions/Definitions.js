8e6229934cb3a683e2bdfb531bde8a3e3e4dbf49
Jaime E Betancourt <jaime.betancourt307@gmail.com>
2021-01-15 15:00:58 -0500

    added components, data,  functionality ,and styling

---
+++ src/components/Definitions/Definitions.js
@@ -0,0 +1,58 @@
+import { light } from '@material-ui/core/styles/createPalette';
+import React from 'react';
+import './Definitions.css';
+
+const Definitions = ({ word, meanings, category, lightMode }) => {
+    return (
+        <div className='meanings'>
+
+            {
+                meanings[0] && word && category === 'en' && (
+                    <audio 
+                        className='audioStyle' 
+                        src={meanings[0].phonetics[0] && meanings[0].phonetics[0].audio}
+                        controls
+                    >
+                        Your browser doesn't support audio element.
+                    </audio>
+                )
+            }
+
+            {
+                word === '' ? <span className='subTitle'>Start by typing a word in Search</span>
+                : (
+                    meanings.map((mean) => 
+                        mean.meanings.map((item) => 
+                            item.definitions.map((def) => (
+                                <div className='singleMeaning' 
+                                    style={{ 
+                                        backgroundColor: lightMode ? '#3b5360' : 'white',
+                                        color: lightMode ? 'white' : 'black'
+                                    }}>
+                                    <b>{def.definition}</b>
+                                    <hr className='hrStyle' />
+
+                                    {
+                                        def.example && (
+                                            <span>
+                                                <b>Example : </b>{def.example}
+                                            </span>
+                                        )
+                                    }
+                                    {def.synonyms && (
+                                        <span>
+                                            <b>Synonyms : </b>{def.synonyms.map((s) => `$(s), `)}
+                                        </span>
+                                    )}
+                                </div>
+                            ))
+                        )
+                    )
+                )
+
+            }
+        </div>
+    )
+}
+
+export default Definitions
---
+++ src/components/Header/Header.js
@@ -0,0 +1,59 @@
+import { createMuiTheme, TextField, ThemeProvider, MenuItem } from '@material-ui/core';
+import React from 'react';
+import './Header.css';
+
+import categories from '../../data/categories';
+
+const Header = ({ category, setCategory, word, setWord, lightMode }) => {
+    const darkTheme = createMuiTheme({
+        palette: {
+            primary: {
+                main: lightMode ? '#000' : '#fff',
+            },
+            type: lightMode ? 'light' : 'dark',
+        },
+    });
+
+    const handleChange = (language) => {
+        setCategory(language);
+        setWord('');
+    }
+
+    return (
+        <div className='header'>
+            <span className='title'>{word ? word : "Word Hunt"}</span>
+
+            <div className='inputs'>
+                <ThemeProvider theme={darkTheme}>
+                    <TextField 
+                        id='standard-basic' 
+                        label='Enter a word' 
+                        className='search' 
+                        value={word}
+                        onChange={(e) =>setWord(e.target.value)}
+                    />
+
+                    <TextField
+                        className='select'
+                        select
+                        label="Langauge"
+                        value={category}
+                        onChange={(e) => handleChange(e.target.value)}
+                    >
+                        {
+                            categories.map((option) => (
+                                <MenuItem key={option.label} value={option.label}>
+                                    {option.value}
+                                </MenuItem>
+                            ))
+                        }
+                            
+                    </TextField>
+
+                </ThemeProvider>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
